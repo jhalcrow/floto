a9a0a229ba79128846eaff44aad58123230f214b
JZ <denial@gmail.com>
2013-03-25 00:17:59 -0400

    Enlarged active image, always shift to left if click image.

--- static/js/controllers.js
+++ static/js/controllers.js
@@ -5,11 +5,20 @@ angular.module('flotoApp', ['ui.bootstrap']);
 
 
 function FlotoCtrl($scope, $http) {
-	$scope.myInterval = 30000;
-	$http.get('pokemon.json').success(function(data) {
-		$scope.images = data;
-		console.log(data);
-	});
+    $scope.myInterval = 30000;
+    $http.get('pokemon.json').success(function(data) {
+        $scope.images = data;
+        console.log(data);
+    });
 
-	
+    $scope.displayImage = function(image) {
+        while($scope.images.indexOf(image) !== 0) {
+            console.log($scope.images);
+            var tempImage = $scope.images[0];
+            $scope.images.shift();
+            $scope.images.push(tempImage);
+        }
+        image.active = true;    
+        console.log("IMAGE");
+    }
 }
