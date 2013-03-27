08066e33cf5f2a103cc856c87baf827dde5b66b0
Jonathan Halcrow <jonathan.halcrow@gmail.com>
2013-03-26 22:45:36 -0400

    Setup script

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='Floto',
+    version='0.1',
+    description='Photo sharing via email',
+    author='Jonathan Halcrow and Josh Zheng',
+    url='http://www.github.com/jhalcrow/floto',
+    packages=['floto']
+)
