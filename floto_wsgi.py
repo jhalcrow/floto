ffceb57f5e258be848a8123b233f5c7543577c82
Jonathan Halcrow <jonathan.halcrow@gmail.com>
2013-03-26 23:02:22 -0400

    Add reverse proxy fixing middleware

--- floto_wsgi.py
+++ floto_wsgi.py
@@ -10,21 +10,48 @@ import logging
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 sys.path.insert(0, PROJECT_ROOT)
 
+class ReverseProxied(object):
+    '''Wrap the application in this middleware and configure the 
+    front-end server to add these headers, to let you quietly bind 
+    this to a URL other than / and to an HTTP scheme that is 
+    different than what is used locally.
+
+    In nginx:
+    location /myprefix {
+        proxy_pass http://192.168.0.1:5001;
+        proxy_set_header Host $host;
+        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+        proxy_set_header X-Scheme $scheme;
+        proxy_set_header X-Script-Name /myprefix;
+        }
+
+    :param app: the WSGI application
+    '''
+    def __init__(self, app):
+        self.app = app
+
+    def __call__(self, environ, start_response):
+        script_name = environ.get('HTTP_X_SCRIPT_NAME', '')
+        if script_name:
+            environ['SCRIPT_NAME'] = script_name
+            path_info = environ['PATH_INFO']
+            if path_info.startswith(script_name):
+                environ['PATH_INFO'] = path_info[len(script_name):]
+
+        scheme = environ.get('HTTP_X_SCHEME', '')
+        if scheme:
+            environ['wsgi.url_scheme'] = scheme
+        return self.app(environ, start_response)
+
 from floto import create_app, config
 application = create_app(config.ProductionConfig())
 from werkzeug.contrib.fixers import ProxyFix
 application.wsgi_app = ProxyFix(application.wsgi_app)
+application.wsgi_app = ReverseProxied(application.wsgi_app)
 
 @application.before_first_request
 def setup_logging():
-    # In production mode, add log handler to sys.stderr.
     application.logger.addHandler(logging.StreamHandler())
     application.logger.setLevel(logging.DEBUG)
     application.logger.addHandler(logging.FileHandler('/tmp/floto.log'))
 
-#from logging.handlers import FileHandler
-#file_handler = FileHandler('/tmp/floto.log')
-
-#file_handler.setLevel(logging.DEBUG)
-#application.logger.addHandler(file_handler
-sys.stderr.write("WTF\n")
