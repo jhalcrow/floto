02dd09580f2e9b9a224ec54e5517e44db14f78ae
JZ <denial@gmail.com>
2013-04-01 02:36:52 -0400

    Emailing picture working, need to fix caption.

--- static/js/controllers.js
+++ static/js/controllers.js
@@ -6,11 +6,21 @@ angular.module('flotoApp', ['ui.bootstrap']);
 
 function FlotoCtrl($scope, $http) {
     $scope.myInterval = 30000;
-    $http.get('pokemon.json').success(function(data) {
-        $scope.images = data;
+    $http.get('/events/test/tip').success(function(data) {
+        $scope.images = data.photos;
         console.log(data);
+        window.setInterval(function(){
+            $http.get('/events/test/new').success(function(data) {
+                for(var photo in data.photos) {
+                    $scope.images.shift();
+                    $scope.images.push(data.photos[photo]);                   
+                }
+            })
+        }, 5000);
     });
 
+
+/*
     $scope.displayImage = function(image) {
         while($scope.images.indexOf(image) !== 0) {
             console.log($scope.images);
@@ -20,5 +30,5 @@ function FlotoCtrl($scope, $http) {
         }
         image.active = true;    
         console.log("IMAGE");
-    }
+    }*/
 }
