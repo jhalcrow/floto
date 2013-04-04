fb4ea8345c1a9f3d6fd127a5999326671484de83
Jonathan Halcrow <jonathan.halcrow@gmail.com>
2013-04-04 07:40:15 -0400

    Fix image orientation

--- floto/util.py
+++ floto/util.py
@@ -2,6 +2,8 @@ from flask import current_app
 import base64
 import bson
 import uuid
+from PIL import Image
+ORIENTATION_TAG = 0x0112 # EXIF Orientation tag
 
 def store_image(event, mandrill_event):
     db = current_app.extensions['mongo']
@@ -20,8 +22,34 @@ def store_image(event, mandrill_event):
                 'subject': email['subject'],
                 'text': email['text'],
             }
-            photo['raw'] = bson.Binary(base64.b64decode(attachment['content']))
+            raw_bytes = base64.b64decode(attachment['content'])
+            img = Image.open(StringIO.StringIO(raw_bytes))
+            img = orient_img(img)
+            photo['raw'] = bson.Binary(img.tostring())
             photo['type'] = attachment['type']
             db.photos.save(photo)
     else:
         current_app.logger.warning('No attachments on email.')
+
+def orient_img(img):
+    '''
+    Reorients an image to EXIF orientation 1 (aka normal)
+    '''
+    device_orientation = img._getexif()[ORIENTATION_TAG]
+    if device_orientation == 2:
+        img = img.transpose(Image.FLIP_LEFT_RIGHT)
+    elif device_orientation == 3:
+        img = img.transpose(Image.ROTATE_180)
+    elif device_orientation == 4:
+        img = img.transpose(Image.FLIP_TOP_BOTTOM)
+    elif device_orientation == 5:
+        img = img.transpose(Image.ROTATE_270).transpose(Image.FLIP_LEFT_RIGHT)
+    elif device_orientation == 6:
+        img = img.transpose(Image.ROTATE_270)
+    elif device_orientation == 7:
+        img = img.transpose(Image.ROTATE_90).transpose(Image.FLIP_LEFT_RIGHT)
+    elif device_orientation == 8:
+        img = img.transpose(Image.ROTATE_90)
+
+    return img
+
