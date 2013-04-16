55fe2660041b8d29444f34c975effc88cb01cd95
JZ <denial@gmail.com>
2013-04-16 14:46:58 -0400

    timeout.

--- static/js/controllers.js
+++ static/js/controllers.js
@@ -12,9 +12,13 @@ function FlotoCtrl($scope, $http) {
             $http.get('/floto/api/events/test/new').success(function(data) {
                 for(var photo in data.photos) {
                     if($scope.images[0].active === true) {
+                        console.log("first.")
                         $scope.images[1].active = true;
-                        $scope.images.push(data.photos[photo]);
-                        $scope.images.shift();
+                        window.setTimeout(function() {
+                            $scope.images.push(data.photos[photo]);
+                            $scope.images.shift();                           
+                        }, 500);
+
                     } else {
                         $scope.images.push(data.photos[photo]);
                         $scope.images.shift();                       
