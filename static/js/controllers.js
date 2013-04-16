42a9b98e3073d2cbcab203a66f127d3f36d321d4
JZ <denial@gmail.com>
2013-04-16 12:36:13 -0400

    Bug.

--- static/js/controllers.js
+++ static/js/controllers.js
@@ -4,20 +4,21 @@
 angular.module('flotoApp', ['ui.bootstrap']);
 
 function FlotoCtrl($scope, $http) {
-    $scope.myInterval = 30000;
+    $scope.myInterval = 3000;
     $http.get('/floto/api/events/test/tip?n=11').success(function(data) {
         $scope.images = data.photos;
 
         window.setInterval(function(){
             $http.get('/floto/api/events/test/new').success(function(data) {
                 for(var photo in data.photos) {
+                    console.log($scope.images[0]);
                     if($scope.images[$scope.images.length].active === false) {
                         $scope.images.unshift(data.photos[photo]);
                         $scope.images.pop();                   
                     }
                 }
             })
-        }, 5000);
+        }, 6000);
     });
 
     $scope.displayImage = function(image) {
