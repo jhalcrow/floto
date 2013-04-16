ee459d6e42912053e0a20b56048cb5aa7f1347d3
JZ <denial@gmail.com>
2013-04-16 11:48:47 -0400

    Code cleanup.

--- static/js/controllers.js
+++ static/js/controllers.js
@@ -3,12 +3,11 @@
 /* Controllers */
 angular.module('flotoApp', ['ui.bootstrap']);
 
-
 function FlotoCtrl($scope, $http) {
     $scope.myInterval = 30000;
     $http.get('/floto/api/events/test/tip?n=11').success(function(data) {
         $scope.images = data.photos;
-        console.log(data);
+
         window.setInterval(function(){
             $http.get('/floto/api/events/test/new').success(function(data) {
                 for(var photo in data.photos) {
