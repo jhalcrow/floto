d2979551ccf41cadac895cbe4e54f83def7368a1
JZ <denial@gmail.com>
2013-04-16 12:52:18 -0400

    New way to change pictures.

--- static/js/controllers.js
+++ static/js/controllers.js
@@ -4,21 +4,19 @@
 angular.module('flotoApp', ['ui.bootstrap']);
 
 function FlotoCtrl($scope, $http) {
-    $scope.myInterval = 5000;
+    $scope.myInterval = 50000000000;
     $http.get('/floto/api/events/test/tip?n=11').success(function(data) {
         $scope.images = data.photos;
 
         window.setInterval(function(){
             $http.get('/floto/api/events/test/new').success(function(data) {
                 for(var photo in data.photos) {
-                    console.log($scope.images[0]);
-                    if($scope.images[$scope.images.length-1].active === false) {
-                        $scope.images.unshift(data.photos[photo]);
-                        $scope.images.pop();                   
-                    }
+                    $scope.images.push(data.photos[photo]);
+                    $scope.images[1].active = true
+                    $scope.images.shift();                   
                 }
             })
-        }, 7000);
+        }, 3000);
     });
 
     $scope.displayImage = function(image) {
